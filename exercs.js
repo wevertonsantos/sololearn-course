f6571820b8af63ccd1d8e94d92667220fcbbf952
Weverton dos Santos <wever-santos@hotmail.com>
2023-05-28 18:07:37 -0300

    newprice

--- exercs.js
+++ exercs.js
@@ -36,3 +36,35 @@ function Flight(flightNumber, flightStatus) {
     this.number = flightNumber;
     this.status = flightStatus;
 };
+
+/*
+A store manager needs a program to set discounts for products.
+
+The program should take the product ID, price and discount as input and output the discounted price.
+However, the changePrice method, which should calculate the discount, is incomplete. Fix it!
+*/
+
+function main() {
+    var prodID = readLine();
+    var price = parseInt(readLine(),10);
+    var discount = parseInt(readLine(),10);
+    
+    var prod1= new Product(prodID, price);
+    console.log(prod1.prodID + " price: " + prod1.price);
+    
+    prod1.changePrice(discount);
+    console.log(prod1.prodID + " new price: " + prod1.price);
+}
+
+function Product(prodID, price) {
+    this.prodID = prodID;
+    this.price = price;
+
+    this.changePrice = function(discount) {
+        //your code goes here
+        var discount = (this.price-(0.15*this.price));
+        this.price = discount;
+    }
+}
+
+prod1.changePrice()
